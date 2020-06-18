c38aaae152e1fadb2164f7547ec5068166436831
Amila Welihinda <amilajack@gmail.com>
2020-06-17 19:54:51 -0700

    allow mutating params

--- typescript.js
+++ typescript.js
@@ -24,6 +24,7 @@ module.exports = {
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-var-requires": "off",
+    "no-param-reassign": ["error", { props: false }],
   },
   settings: {
     "import/parsers": {
