31b309f1da99e1e30972c5f19fec65f91b4d4990
Amila Welihinda <amilajack@gmail.com>
2020-06-17 19:08:53 -0700

    add "@typescript-eslint/explicit-module-boundary-types" rule

--- typescript.js
+++ typescript.js
@@ -22,6 +22,7 @@ module.exports = {
   },
   rules: {
     "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-var-requires": "off",
   },
   settings: {
