953f19d8a0b870d56d7f516ca40c1f481074d669
Amila Welihinda <amilajack@gmail.com>
2020-11-07 09:18:58 -0800

    fix: initial passing tests

---
+++ packages/eslint-config-erb/test/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    jest: true
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+}
